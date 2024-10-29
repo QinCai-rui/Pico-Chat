5b7e2591a79554978c35294472901e99d9c98464
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-10-30 10:57:00 +1300

    Create forbid.py
    HTML responses for 401 and 403 codes

---
+++ forbid.py
@@ -0,0 +1,22 @@
+HTML_FOR_403 = '''
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>403 Forbidden</h1>
+<p>You don't have permission to access this resource.</p>
+<hr>
+<address>MicroPython (RP2040) Server at {server_ip} Port 80</address>
+</body></html>
+'''
+
+HTML_FOR_401 = '''<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>401 Unauthorised</title>
+</head><body>
+<h1>401 Unauthorised</h1>
+<p>You don't have permission to access this resource. Please contact application owner for more information or for access.</p>
+<hr>
+<address>MicroPython (RP2040) Server at {server_ip} Port 80</address>
+</body></html>
+'''
